89f80067bb4fde0d1042f998daaa410c93565717
Brian Summa <brian.f.summa@chesa.com>
2026-04-28 11:53:54 -0500

    chore: remove unused git-wrapper.ts

--- src/utils/git-wrapper.ts
+++
@@ -1,5 +0,0 @@
-import { execFileSync, ExecFileSyncOptions } from "node:child_process";
-
-export function execGit(args: string[], options: ExecFileSyncOptions & { encoding: "utf-8" }): string {
-  return execFileSync("git", args, options).toString().trim();
-}
