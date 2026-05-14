63215212003716554743a7e39a5d550da33b5791
Brian Summa <brian.f.summa@chesa.com>
2026-05-14 18:35:11 -0500

    fix: WR-03 remove unused spawnSync import from daemon-cmd.ts

--- src/cli/daemon-cmd.ts
+++ src/cli/daemon-cmd.ts
@@ -1,4 +1,4 @@
-import { execFileSync, spawnSync } from "node:child_process";
+import { execFileSync } from "node:child_process";
 import * as fs from "node:fs";
 import * as net from "node:net";
 import * as path from "node:path";
